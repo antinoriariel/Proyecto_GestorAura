0d4ccfdfe377ad09e5292a244b4e3251933ff552
= <leonelfalegre@gmail.com>
2025-09-10 11:07:48 -0300

    El panel1 se establecio con la propiedad dock=left para fijarlo y ajustar el tamaño del contenedor mdi

--- CapaPresentacion/MdiDashboard.Designer.cs
+++ CapaPresentacion/MdiDashboard.Designer.cs
@@ -56,22 +56,23 @@
             toolStripStatusLabel = new ToolStripStatusLabel();
             toolTip = new ToolTip(components);
             panel1 = new Panel();
-            panel3 = new Panel();
-            label1 = new Label();
             pictureBox1 = new PictureBox();
+            label1 = new Label();
+            panel3 = new Panel();
             menuStrip.SuspendLayout();
             statusStrip.SuspendLayout();
-            panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip
             // 
+            menuStrip.ImageScalingSize = new Size(20, 20);
             menuStrip.Items.AddRange(new ToolStripItem[] { sistemaToolStripMenuItem, editMenu, viewMenu, toolsMenu, helpMenu, toolStripMenuItem2 });
             menuStrip.Location = new Point(0, 0);
             menuStrip.Name = "menuStrip";
-            menuStrip.Padding = new Padding(7, 2, 0, 2);
-            menuStrip.Size = new Size(737, 24);
+            menuStrip.Padding = new Padding(8, 3, 0, 3);
+            menuStrip.Size = new Size(842, 30);
             menuStrip.TabIndex = 0;
             menuStrip.Text = "MenuStrip";
             menuStrip.ItemClicked += menuStrip_ItemClicked;
@@ -80,26 +81,26 @@
             // 
             sistemaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem1, salirToolStripMenuItem });
             sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
-            sistemaToolStripMenuItem.Size = new Size(60, 20);
+            sistemaToolStripMenuItem.Size = new Size(75, 24);
             sistemaToolStripMenuItem.Text = "Sistema";
             // 
             // toolStripMenuItem1
             // 
             toolStripMenuItem1.Name = "toolStripMenuItem1";
-            toolStripMenuItem1.Size = new Size(180, 22);
+            toolStripMenuItem1.Size = new Size(225, 26);
             toolStripMenuItem1.Text = "toolStripMenuItem1";
             // 
             // salirToolStripMenuItem
             // 
             salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            salirToolStripMenuItem.Size = new Size(180, 22);
+            salirToolStripMenuItem.Size = new Size(225, 26);
             salirToolStripMenuItem.Text = "Salir";
             // 
             // editMenu
             // 
             editMenu.DropDownItems.AddRange(new ToolStripItem[] { undoToolStripMenuItem, redoToolStripMenuItem, toolStripSeparator6, cutToolStripMenuItem, copyToolStripMenuItem, pasteToolStripMenuItem, toolStripSeparator7 });
             editMenu.Name = "editMenu";
-            editMenu.Size = new Size(49, 20);
+            editMenu.Size = new Size(62, 24);
             editMenu.Text = "&Editar";
             // 
             // undoToolStripMenuItem
@@ -108,7 +109,7 @@
             undoToolStripMenuItem.ImageTransparentColor = Color.Black;
             undoToolStripMenuItem.Name = "undoToolStripMenuItem";
             undoToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Z;
-            undoToolStripMenuItem.Size = new Size(163, 22);
+            undoToolStripMenuItem.Size = new Size(204, 26);
             undoToolStripMenuItem.Text = "&Deshacer";
             // 
             // redoToolStripMenuItem
@@ -117,13 +118,13 @@
             redoToolStripMenuItem.ImageTransparentColor = Color.Black;
             redoToolStripMenuItem.Name = "redoToolStripMenuItem";
             redoToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Y;
-            redoToolStripMenuItem.Size = new Size(163, 22);
+            redoToolStripMenuItem.Size = new Size(204, 26);
             redoToolStripMenuItem.Text = "&Rehacer";
             // 
             // toolStripSeparator6
             // 
             toolStripSeparator6.Name = "toolStripSeparator6";
-            toolStripSeparator6.Size = new Size(160, 6);
+            toolStripSeparator6.Size = new Size(201, 6);
             // 
             // cutToolStripMenuItem
             // 
@@ -131,7 +132,7 @@
             cutToolStripMenuItem.ImageTransparentColor = Color.Black;
             cutToolStripMenuItem.Name = "cutToolStripMenuItem";
             cutToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.X;
-            cutToolStripMenuItem.Size = new Size(163, 22);
+            cutToolStripMenuItem.Size = new Size(204, 26);
             cutToolStripMenuItem.Text = "Cor&tar";
             cutToolStripMenuItem.Click += CutToolStripMenuItem_Click;
             // 
@@ -141,7 +142,7 @@
             copyToolStripMenuItem.ImageTransparentColor = Color.Black;
             copyToolStripMenuItem.Name = "copyToolStripMenuItem";
             copyToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.C;
-            copyToolStripMenuItem.Size = new Size(163, 22);
+            copyToolStripMenuItem.Size = new Size(204, 26);
             copyToolStripMenuItem.Text = "&Copiar";
             copyToolStripMenuItem.Click += CopyToolStripMenuItem_Click;
             // 
@@ -151,20 +152,20 @@
             pasteToolStripMenuItem.ImageTransparentColor = Color.Black;
             pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
             pasteToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.V;
-            pasteToolStripMenuItem.Size = new Size(163, 22);
+            pasteToolStripMenuItem.Size = new Size(204, 26);
             pasteToolStripMenuItem.Text = "&Pegar";
             pasteToolStripMenuItem.Click += PasteToolStripMenuItem_Click;
             // 
             // toolStripSeparator7
             // 
             toolStripSeparator7.Name = "toolStripSeparator7";
-            toolStripSeparator7.Size = new Size(160, 6);
+            toolStripSeparator7.Size = new Size(201, 6);
             // 
             // viewMenu
             // 
             viewMenu.DropDownItems.AddRange(new ToolStripItem[] { toolBarToolStripMenuItem, statusBarToolStripMenuItem });
             viewMenu.Name = "viewMenu";
-            viewMenu.Size = new Size(35, 20);
+            viewMenu.Size = new Size(44, 24);
             viewMenu.Text = "&Ver";
             // 
             // toolBarToolStripMenuItem
@@ -173,7 +174,7 @@
             toolBarToolStripMenuItem.CheckOnClick = true;
             toolBarToolStripMenuItem.CheckState = CheckState.Checked;
             toolBarToolStripMenuItem.Name = "toolBarToolStripMenuItem";
-            toolBarToolStripMenuItem.Size = new Size(189, 22);
+            toolBarToolStripMenuItem.Size = new Size(238, 26);
             toolBarToolStripMenuItem.Text = "&Barra de herramientas";
             // 
             // statusBarToolStripMenuItem
@@ -182,7 +183,7 @@
             statusBarToolStripMenuItem.CheckOnClick = true;
             statusBarToolStripMenuItem.CheckState = CheckState.Checked;
             statusBarToolStripMenuItem.Name = "statusBarToolStripMenuItem";
-            statusBarToolStripMenuItem.Size = new Size(189, 22);
+            statusBarToolStripMenuItem.Size = new Size(238, 26);
             statusBarToolStripMenuItem.Text = "&Barra de estado";
             statusBarToolStripMenuItem.Click += StatusBarToolStripMenuItem_Click;
             // 
@@ -190,13 +191,13 @@
             // 
             toolsMenu.DropDownItems.AddRange(new ToolStripItem[] { optionsToolStripMenuItem });
             toolsMenu.Name = "toolsMenu";
-            toolsMenu.Size = new Size(90, 20);
+            toolsMenu.Size = new Size(112, 24);
             toolsMenu.Text = "&Herramientas";
             // 
             // optionsToolStripMenuItem
             // 
             optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            optionsToolStripMenuItem.Size = new Size(124, 22);
+            optionsToolStripMenuItem.Size = new Size(154, 26);
             optionsToolStripMenuItem.Text = "&Opciones";
             optionsToolStripMenuItem.Click += optionsToolStripMenuItem_Click;
             // 
@@ -204,7 +205,7 @@
             // 
             helpMenu.DropDownItems.AddRange(new ToolStripItem[] { indexToolStripMenuItem, toolStripSeparator8, aboutToolStripMenuItem });
             helpMenu.Name = "helpMenu";
-            helpMenu.Size = new Size(53, 20);
+            helpMenu.Size = new Size(65, 24);
             helpMenu.Text = "Ay&uda";
             // 
             // indexToolStripMenuItem
@@ -212,98 +213,102 @@
             indexToolStripMenuItem.Image = (Image)resources.GetObject("indexToolStripMenuItem.Image");
             indexToolStripMenuItem.ImageTransparentColor = Color.Black;
             indexToolStripMenuItem.Name = "indexToolStripMenuItem";
-            indexToolStripMenuItem.Size = new Size(147, 22);
+            indexToolStripMenuItem.Size = new Size(180, 26);
             indexToolStripMenuItem.Text = "&Manual";
             indexToolStripMenuItem.Click += indexToolStripMenuItem_Click;
             // 
             // toolStripSeparator8
             // 
             toolStripSeparator8.Name = "toolStripSeparator8";
-            toolStripSeparator8.Size = new Size(144, 6);
+            toolStripSeparator8.Size = new Size(177, 6);
             // 
             // aboutToolStripMenuItem
             // 
             aboutToolStripMenuItem.Image = Properties.Resources.medic_1;
             aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            aboutToolStripMenuItem.Size = new Size(147, 22);
+            aboutToolStripMenuItem.Size = new Size(180, 26);
             aboutToolStripMenuItem.Text = "&Acerca de... ...";
             aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
             // 
             // toolStripMenuItem2
             // 
             toolStripMenuItem2.Name = "toolStripMenuItem2";
-            toolStripMenuItem2.Size = new Size(12, 20);
+            toolStripMenuItem2.Size = new Size(14, 24);
             // 
             // statusStrip
             // 
+            statusStrip.ImageScalingSize = new Size(20, 20);
             statusStrip.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel });
-            statusStrip.Location = new Point(0, 501);
+            statusStrip.Location = new Point(0, 671);
             statusStrip.Name = "statusStrip";
-            statusStrip.Padding = new Padding(1, 0, 16, 0);
-            statusStrip.Size = new Size(737, 22);
+            statusStrip.Padding = new Padding(1, 0, 18, 0);
+            statusStrip.Size = new Size(842, 26);
             statusStrip.TabIndex = 2;
             statusStrip.Text = "StatusStrip";
             // 
             // toolStripStatusLabel
             // 
             toolStripStatusLabel.Name = "toolStripStatusLabel";
-            toolStripStatusLabel.Size = new Size(42, 17);
+            toolStripStatusLabel.Size = new Size(54, 20);
             toolStripStatusLabel.Text = "Estado";
             // 
             // panel1
             // 
-            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             panel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             panel1.BackColor = SystemColors.ControlDarkDark;
-            panel1.Location = new Point(0, 144);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 30);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(215, 354);
+            panel1.Size = new Size(246, 641);
             panel1.TabIndex = 4;
             panel1.Paint += panel1_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.medic_1;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(79, 16);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(93, 88);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(79, 108);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Gestor Medic";
+            label1.Click += label1_Click;
             // 
             // panel3
             // 
             panel3.BackColor = SystemColors.HighlightText;
             panel3.Controls.Add(label1);
             panel3.Controls.Add(pictureBox1);
-            panel3.Location = new Point(0, 27);
+            panel3.Location = new Point(0, 36);
+            panel3.Margin = new Padding(3, 4, 3, 4);
             panel3.Name = "panel3";
-            panel3.Size = new Size(215, 111);
+            panel3.Size = new Size(246, 148);
             panel3.TabIndex = 0;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(69, 81);
-            label1.Name = "label1";
-            label1.Size = new Size(77, 15);
-            label1.TabIndex = 1;
-            label1.Text = "Gestor Medic";
-            label1.Click += label1_Click;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BackgroundImage = Properties.Resources.medic_1;
-            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox1.Location = new Point(69, 12);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(81, 66);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
             // 
             // MdiDahsboard
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(737, 523);
+            ClientSize = new Size(842, 697);
             Controls.Add(panel3);
             Controls.Add(panel1);
             Controls.Add(statusStrip);
             Controls.Add(menuStrip);
             IsMdiContainer = true;
             MainMenuStrip = menuStrip;
-            Margin = new Padding(4, 3, 4, 3);
+            Margin = new Padding(5, 4, 5, 4);
             Name = "MdiDahsboard";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Medic - Administracion general";
@@ -313,9 +318,9 @@
             menuStrip.PerformLayout();
             statusStrip.ResumeLayout(false);
             statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel3.ResumeLayout(false);
             panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
 
@@ -356,9 +361,9 @@
         private ToolStripMenuItem salirToolStripMenuItem;
         private ToolStripMenuItem toolStripMenuItem1;
         private Panel panel1;
-        private Panel panel3;
-        private Label label1;
         private PictureBox pictureBox1;
+        private Label label1;
+        private Panel panel3;
     }
 }
 
